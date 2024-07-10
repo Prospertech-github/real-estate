df3770a98bebd16efe98ce64c3f678d436be25f6
Prosper Ikechukwu <ikechukwuprosper8@gmail.com>
2024-07-10 12:53:02 +0100

    FEAT: Completed folder structure and imported Google Font

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,26 @@
-import "./globals.css";
-
+import "./_styles/globals.css";
+import {Lora} from 'next/font/google'
 
 export const metadata = {
   title: "My Real Estate Website",
 };
 
+const lora = Lora({
+  weight: ['400', '700', '500', '600'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={lora.className}>
+      <body>
+        <h1> Whereas recognition of the inherent dignity</h1>
+        {children}
+      </body>
     </html>
   );
 }
+
+
